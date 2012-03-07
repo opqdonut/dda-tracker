c92b89fd555c1330e23499f59e378139ba2f60ae
Joel E Kaasinen <jkaasine@svm-19.cs.helsinki.fi>
2012-03-07 15:29:58 +0200

    a lot more

---
+++ Model.hs
@@ -0,0 +1,29 @@
+module Model where
+
+import Database.HDBC.Sqlite3 (connectSqlite3)
+import Database.HDBC
+import Control.Monad
+import qualified Data.Map as M
+
+prepDB :: IConnection conn => conn -> IO ()
+prepDB dbh =
+    do tables <- getTables dbh
+       when (not ("answers" `elem` tables)) $
+           do run dbh "CREATE TABLE answers (\
+	      	       \user TEXT NOT NULL,\
+                       \question TEXT NOT NULL,\
+                       \answer TEST NOT NULL,\
+                       \PRIMARY KEY (user,question))" []
+              return ()
+       commit dbh
+
+getAnswers :: IConnection conn => conn -> String -> IO (M.Map String String)
+getAnswers conn name = 
+    do rows <- quickQuery' conn "SELECT * FROM answers WHERE user = ?" [toSql name]
+       return . M.fromList $ [(fromSql a,fromSql b) | [_,a,b] <- rows]
+
+putAnswers :: IConnection conn => conn -> String -> M.Map String String -> IO ()
+putAnswers conn name ans =
+    do s <- prepare conn "insert into answers values (?,?,?)"
+       executeMany s [ map toSql [name, q, a] | (q,a) <- M.assocs ans ]
+       commit conn
--- foo.hs
+++ foo.hs
@@ -1,22 +1,42 @@
 import Network.CGI
 import Text.XHtml
+import Database.HDBC.Sqlite3 (connectSqlite3) 
+import qualified Data.Map as M
+
+import Model
+
+nameForm = return $ form << [paragraph << ("My name is " +++ textfield "name"),
+    	                     submit "" "Submit"]
  
-nameForm = form << [paragraph << ("My name is " +++ textfield "name"),
-    	            submit "" "Submit"]
- 
-questions = ["q"++show i | i <- [1..3]]
+questions = ["ex"++show i | i <- [1..3]]
 answers = ["a"++show i | i <- [1..6]]
 
-queryForm name = form << [textfield "name" ! [strAttr "hidden" "true", strAttr "value" name],
-                          ulist << [li << (q +++ [radio q a | a<-answers]) | q<-questions],
-	       	 	  submit "" "Submit"]
+getAnswer q = do x <- getInput q
+                 return (q,x)
 
-greet n = paragraph << ("Hello " ++ n ++ "!") +++ queryForm n
- 
+processAnswers conn user = do ans <- mapM getAnswer questions
+                              let ans' = M.fromList [(q,a) | (q,Just a) <- ans]
+			      liftIO $ putAnswers conn user ans'
+
+queryForm conn name = 
+    do processAnswers conn name
+       ans <- liftIO $ getAnswers conn name
+       let r q a | M.lookup q ans == Just a   = radio q a ! [strAttr "checked" "true"]
+       	         | otherwise                  = radio q a
+       return $ paragraph << ("Hello " ++ name ++ "!") 
+                +++ pre << show ans
+                +++
+       	        form << [textfield "name" ! [strAttr "hidden" "true", strAttr "value" name],
+               	         ulist << [li << (q +++ [r q a | a<-answers]) | q<-questions],
+	       	         submit "" "Save"]
+
 page t b = header << thetitle << t +++ body << b
- 
-cgiMain = do mn <- getInput "name"
-             let x = maybe nameForm greet mn
-             output . renderHtml $ page "Input example" x
+
+cgiMain :: CGI CGIResult 
+cgiMain = do c <- liftIO $ connectSqlite3 "answers.db"
+	     liftIO $ prepDB c
+             mn <- getInput "name"
+             x <- maybe nameForm (queryForm c) mn
+             output . renderHtml $ page "Questionnaire" x
  
 main = runCGI $ handleErrors cgiMain
