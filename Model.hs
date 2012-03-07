663871f9996482014f06f65581b2ec10d9c2601a
Joel E Kaasinen <jkaasine@svm-19.cs.helsinki.fi>
2012-03-07 17:23:45 +0200

    stats, styleable

--- Model.hs
+++ Model.hs
@@ -19,11 +19,16 @@ prepDB dbh =
 
 getAnswers :: IConnection conn => conn -> String -> IO (M.Map String String)
 getAnswers conn name = 
-    do rows <- quickQuery' conn "SELECT * FROM answers WHERE user = ?" [toSql name]
-       return . M.fromList $ [(fromSql a,fromSql b) | [_,a,b] <- rows]
+    do rows <- quickQuery' conn "SELECT question, answer FROM answers WHERE user = ?" [toSql name]
+       return . M.fromList $ [(fromSql a,fromSql b) | [a,b] <- rows]
 
 putAnswers :: IConnection conn => conn -> String -> M.Map String String -> IO ()
 putAnswers conn name ans =
     do s <- prepare conn "insert or replace into answers values (?,?,?)"
        executeMany s [ map toSql [name, q, a] | (q,a) <- M.assocs ans ]
        commit conn
+
+getStats :: IConnection conn => conn -> IO (M.Map (String,String) Int)
+getStats conn =
+    do s <- quickQuery' conn "SELECT question,answer,count(*) FROM answers GROUP BY question,answer" []
+       return . M.fromList $ [((fromSql a, fromSql b), fromSql c) | [a,b,c] <- s]
--- foo.hs
+++ foo.hs
@@ -6,12 +6,23 @@ import qualified Data.Map as M
 import Model
 
 nameForm c = do s <- scriptName
-                return $ form ! [strAttr "method" "get", strAttr "action" s]
-                  << [paragraph << ("Name: " +++ textfield "name"),
-    	       	      submit "" "Submit"]
+	        stats <- liftIO $ getStats c
+                return $
+                  form ! [strAttr "method" "get", strAttr "action" s]
+                    << [paragraph << ("Name: " +++ textfield "name"),
+    	       	        submit "" "Submit"]
+                  +++
+                  qtable (stat stats)
  
 questions = ["ex"++show i | i <- [1..3]]
 answers = ["a"++show i | i <- [1..6]]
+
+qtable f = table << [h, concatHtml $ map row questions]
+  where h = tr ! [strAttr "id" "header"] << map (td<<) ("":answers)
+        row q  = tr ! [strAttr "class" q] << [td << q, concatHtml $ map (cell q) answers]
+        cell q a = td ! [strAttr "id" a] << f q a
+
+stat s q a = thespan ! [strAttr "class" "count"] << show (M.findWithDefault 0 (q,a) s)
 
 getAnswer q = do x <- getInput q
                  return (q,x)
@@ -23,14 +34,15 @@ processAnswers conn user = do ans <- mapM getAnswer questions
 queryForm conn name = 
     do 
        ans <- liftIO $ getAnswers conn name
-       let r q a | M.lookup q ans == Just a   = radio q a ! [strAttr "checked" "true"]
-       	         | otherwise                  = radio q a
+       stats <- liftIO $ getStats conn
+       let r q a | M.lookup q ans == Just a   = radio q a ! [strAttr "checked" "true"] +++ stat stats q a
+       	         | otherwise                  = radio q a +++ stat stats q a
        s <- scriptName
        return $ paragraph << ("Hello " ++ name ++ "!") 
                 +++ pre << show ans
                 +++
        	        form ! [strAttr "method" "post", strAttr "action" (s++"/"++name)]
-		   << [ulist << [li << (q +++ [r q a | a<-answers]) | q<-questions],
+		   << [qtable r,
 	       	       submit "" "Save"]
 
 userPage conn name = do m <- requestMethod
