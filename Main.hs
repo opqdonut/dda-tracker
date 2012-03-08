d5d925c8ee2ce3b7e0768b716bd5ed0d1e902ba2
Jukka Suomela <jukka.suomela@iki.fi>
2012-03-08 23:01:11 +0200

    Real questions and answers; HTML class names.
    className function attempts to translate arbitrary strings to valid HTML class names.

--- Main.hs
+++ Main.hs
@@ -8,12 +8,17 @@ import qualified Data.Map as M
 
 import Model
 
-questions = ["ex"++show i | i <- [1..3]]
-answers = ["a"++show i | i <- [1..6]]
+questions = ["1."++show i | i <- [1..10]] ++ ["2."++show i | i <- [1..10]] ++ ["3."++show i | i <- [1..15]]
+answers = ["-", "A", "B", "C", "D", "E", "X"]
+
+dClassName d [] = []
+dClassName d (c:s) | c `elem` ['a'..'z']++['A'..'Z']++['0'..'9'] = d ++ [c] ++ dClassName "" s
+                   | otherwise = dClassName "-" s
+className prefix suffix = prefix ++ dClassName "-" suffix
 
 qtable f = table << [h, concatHtml $ map row questions]
   where h = tr ! [theclass "header"] << map (td<<) ("":answers)
-        row q  = tr ! [theclass q] << [td ! [theclass "sider"] << q,
+        row q  = tr ! [theclass (className "q" q)] << [td ! [theclass "sider"] << q,
                                               concatHtml $ map (f q) answers]
 
 stat s q a = thespan ! [theclass "count"] << show (M.findWithDefault 0 (q,a) s)
@@ -34,9 +39,9 @@ userHtml conn name =
     do ans <- liftIO $ getAnswers conn name
        stats <- liftIO $ getStats conn
        let r q a
-             | M.lookup q ans == Just a = td ! [theclass (a++" selected")]
+             | M.lookup q ans == Just a = td ! [theclass ((className "a" a)++" selected")]
                                           << [radio q a ! [checked], stat stats q a]
-             | otherwise                = td ! [theclass a]
+             | otherwise                = td ! [theclass (className "a" a)]
                                           << [radio q a, stat stats q a]
        u <- userUrl name
        return $ paragraph << ("Hello " ++ name ++ "!") 
@@ -56,7 +61,7 @@ mainHtml c = do s <- scriptName
                     << [paragraph << ("Name: " +++ textfield "name"),
                         submit "" "Login"]
                   +++
-                  qtable (\q a -> td ! [theclass a] << stat stats q a)
+                  qtable (\q a -> td ! [theclass (className "a" a)] << stat stats q a)
 
 mainPage conn = do mn <- getInput "name"
                    case mn of Nothing -> mainHtml conn >>= out
